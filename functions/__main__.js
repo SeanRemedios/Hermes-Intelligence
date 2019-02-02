9eb4fe3f841f82596890d83a9f7dd31aba57a2d9
SeanRemedios <14sr84@queensu.ca>
2019-02-02 15:20:47 -0500

    Added taskwrapper calls

--- functions/__main__.js
+++ functions/__main__.js
@@ -6,14 +6,29 @@ const templatePath = __dirname + '/../static/index.ejs';
 * A basic app
 * @returns {Buffer}
 */
-module.exports = (context, callback) => {
-  return ejs.renderFile(
-    templatePath,
-    {
-      SERVICE_PATH: context.service.identifier
-    },
-    {},
-    (err, response) =>
-      callback(err, new Buffer.from(response || ''), { 'Content-Type': 'text/html' })
-  );
+module.exports = (objectid='', context, callback) => {
+	if (objectid != '') {
+		const NOTIFICATION = 'notificiations.notification';
+		lib[`${context.service.identifier}.${NOTIFICATION}`](
+			objectid,
+			(err, result) => {
+				let message = err ? err.message : result;
+				send(
+					from.number,
+					message,
+					to.number,
+					callback
+				);
+			});
+	}
+
+  	return ejs.renderFile(
+		templatePath,
+		{
+			SERVICE_PATH: context.service.identifier
+		},
+		{},
+		(err, response) =>
+			callback(err, new Buffer.from(response || ''), { 'Content-Type': 'text/html' })
+  	);
 };
